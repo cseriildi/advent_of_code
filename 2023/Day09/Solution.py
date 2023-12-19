fec16d83879e9599ded2e40bcfe78234b6004ea2
Ildikó Cseri <cseriildiko@icloud.com>
2023-12-19 10:55:32 +0100

    Update Solution

--- 2023/Day09/Solution.py
+++ 2023/Day09/Solution.py
@@ -9,23 +9,18 @@ input = [[int(i) for i in line.split()] for line in f.read().splitlines()]
 ## Defining function ##
 #######################
 
-def predicting(histories, direction ):
+def predicting(history, direction = 1):
     
-    prediction =  0
-    for history in histories:
+    map = [history]
+    while list(set(map[-1])) != [0]:
+        map.append([map[-1][k + 1] - map[-1][k] for k in range(len(map[-1]) - 1)])
 
-        map = [history]
-        while list(set(map[-1])) != [0]:
-            map.append([map[-1][k + 1] - map[-1][k] for k in range(len(map[-1]) - 1)])
-
-        p = 0
-        for i in range(2, len(map)+1):
-            if direction == 1:
-                p += map[-i][-1] 
-            else:
-                p = map[-i][0] - p
-
-        prediction += p
+    prediction = 0
+    for i in range(2, len(map)+1):
+        if direction == 1:
+            prediction += map[-i][-1] 
+        else:
+            prediction = map[-i][0] - prediction
 
     return prediction  
 
@@ -33,12 +28,15 @@ def predicting(histories, direction ):
 ## PART 1 SOLUTION ##
 #####################
 
-next = 1
-print( "Part 1 Solution: ", predicting(input, next))
+prediction = sum([predicting(history) for history in input])
+
+print( "Part 1 Solution: ", prediction)
 
 #####################
 ## PART 2 SOLUTION ##
 #####################
 
 previous = -1
-print( "Part 2 Solution: ", predicting(input, previous))
+prediction = sum([predicting(history, previous) for history in input])
+
+print( "Part 2 Solution: ", prediction)
