973091a7d7e9d521be3cbc5da4eb7362f5423413
Ildikó Cseri <cseriildiko@icloud.com>
2023-12-22 21:20:39 +0100

    Update Solution

--- 2023/Day16/Solution.py
+++ 2023/Day16/Solution.py
@@ -10,47 +10,52 @@ input = f.read().splitlines()
 #########################
 
 def vector_distance(first, second = (0, 0)):
-    return [first[0] + second[0],  first[1] + second[1]]
+    return (first[0] + second[0],  first[1] + second[1])
 
-def count_energy(start_list, pattern):
+def count_energy(start, pattern):
     max_row = len(pattern)
     max_column = len(pattern[0])
+
+    right = (0, 1)
+    left = (0, -1)
+    up = (-1, 0)
+    down = (1, 0)
   
-    symbols = {"\\": {(0, 1): [(1, 0)], (-1, 0): [(0, -1)], (0, -1): [(-1, 0)], (1, 0): [(0, 1)]},
-           "/": {(0, 1): [(-1, 0)], (-1, 0): [(0, 1)], (0, -1):[(1, 0)], (1, 0):[(0, -1)]},
-           "|": {(0, 1): [(-1, 0), (1, 0)], (0, -1): [(-1, 0), (1, 0)], (-1, 0):[(-1, 0)], (1, 0): [(1, 0)]},
-           "-": {(-1, 0): [(0, 1), (0, -1)], (1, 0): [(0, 1), (0, -1)], (0, -1): [(0, -1)], (0, 1): [(0, 1)]},
-           ".": {(-1, 0): [(-1, 0)], (1, 0): [(1, 0)], (0, -1):[(0, -1)], (0, 1): [(0, 1)]}}
+    symbols = {"\\": {right: [down], up: [left], left: [up], down: [right]},
+           "/": {right: [up], up: [right], left:[down], down:[left]},
+           "|": {right: [up, down], left: [up, down], up:[up], down: [down]},
+           "-": {up: [right, left], down: [right, left], left: [left], right: [right]},
+           ".": {up: [up], down: [down], left:[left], right: [right]}}
     
-    max_energy = 0
- 
-    for j in start_list:
-        coordinates = [j]
+    coordinates = set()
+    been_there = set()
+    to_check = [start]
 
-        for coord in coordinates:
+    for p in to_check:
+        if p not in been_there:
+            been_there.add(p)
 
-            row, column = coord[0]
-            direction = coord[1]
+            row, column = p[0]
+            
+            direction = p[1]
+
             new_directions = symbols[pattern[row][column]][direction]
-
-            for dir in new_directions:
-                new_row, new_column = vector_distance((row, column), dir)
                 
-                if new_row in range(max_row) and new_column in range(max_column) and [(new_row, new_column), dir] not in coordinates:
-                    coordinates.append([(new_row, new_column), dir])
-                elif [(row, column), (dir[0] * -1, dir[1] * -1)] in start_list:
-                    start_list.remove([(row, column), (dir[0] * -1, dir[1] * -1)])
+            for dir in new_directions:
 
+                new_row, new_column = vector_distance((row, column), dir)
 
-        max_energy = max(len(list(set(i[0] for i in coordinates))), max_energy)
+                if new_row in range(max_row) and new_column in range(max_column):
+                    to_check.append(((new_row, new_column), dir))
 
-    return max_energy
+            coordinates.add((row, column))
+    return len(coordinates)
 
 #####################
 ## PART 1 SOLUTION ##
 #####################
 
-starting_coordinate = [[(0, 0), (0, 1)]]
+starting_coordinate = ((0, 0), (0, 1))
 energy = count_energy(starting_coordinate, input)
 
 print("Part 1 Solution: ", energy)
@@ -58,9 +63,9 @@ print("Part 1 Solution: ", energy)
 #####################
 ## PART 2 SOLUTION ##
 #####################
-print("Part 2 with brute force, running time around 3 minutes, please wait for the result")
-starting_coordinates = [[(0, i), (1, 0)] for i in range(len(input[0]))] + [[(len(input) - 1, i), (-1, 0)] for i in range(len(input[0]))] + [[(i, 0), (0, 1)] for i in range(len(input))] + [[(i, len(input) - 1), (0, -1)] for i in range(len(input))]
 
-energy = count_energy(starting_coordinates, input)
+starting_coordinates = [((0, i), (1, 0)) for i in range(len(input[0]))] + [((len(input) - 1, i), (-1, 0)) for i in range(len(input[0]))] + [((i, 0), (0, 1)) for i in range(len(input))] + [((i, len(input) - 1), (0, -1)) for i in range(len(input))]
+
+energy = max([count_energy(start, input) for start in starting_coordinates])
 
 print("Part 2 Solution: ", energy)
