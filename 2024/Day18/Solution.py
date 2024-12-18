c52ee163cbb5b4ea558fe229ddcd9ed098a0e525
Ildikó Cseri <cseriildiko@icloud.com>
2024-12-18 09:11:06 +0100

    add Grid class

--- 2024/Day18/Solution.py
+++ 2024/Day18/Solution.py
@@ -7,34 +7,59 @@ from heapq import heappush, heappop
 ## INPUT PARSING ##
 ###################
 
+class Grid:
+	def __init__(self, rows, cols, placeholder=".", obstacle="", obstacles=[]):
+		self.rows = rows
+		self.cols = cols
+		self.placeholder = placeholder
+		self.obstacle = obstacle
+		self.grid = np.full((rows, cols), placeholder, dtype="<U1")
+		for i in obstacles:
+			self[i] = obstacle
+
+	def __getitem__(self, key):
+		return self.grid[key]
+
+	def __setitem__(self, key, value):
+		self.grid[key] = value
+
+	def __str__(self):
+		return '\n'.join([''.join(row) for row in self.grid])
+	
+	def add_obstacle(self, point):
+		self[point] = self.obstacle
+
+	def on_board(self, point):
+		return 0 <= point[0] < self.rows and 0 <= point[1] < self.cols
+	
+	def is_obstacle(self, point):
+		return self[point] == self.obstacle
+	
+	def count_elements(self, element):
+		return np.count_nonzero(self.grid == element)
+	
+	def shape(self):
+		return self.grid.shape
+	
 class Data:
-	def __init__(self, filename):
+	def __init__(self, filename, puzzle_type, end, firstx):
 
 		self.filename = filename
 		self.part = 1
-		if filename == EXAMPLE_FILE_NAME:
-			self.type = "Example"
-		elif filename == INPUT_FILE_NAME:
-			self.type = "Solution"
 		self.result = ""
+		self.type = puzzle_type
 		self.start = (0, 0)
-		if filename == EXAMPLE_FILE_NAME:
-			self.end = (6, 6)
-			self.firstx = 12
-		else:
-			self.end = (70, 70)
-			self.firstx = 1024
+		self.end = end
+		self.firstx = firstx
 		self.parse_data()
-		self.grid = np.full((self.end[1] + 1, self.end[0] + 1), ".", dtype="<U1")
-		self.grid[tuple(zip(*self.drops[:self.firstx]))] = "#"
-		self.rows, self.cols = self.grid.shape
+		self.grid = Grid(self.end[0] + 1, self.end[1] + 1, ".", "#", self.drops[:self.firstx])
+		self.rows, self.cols = self.grid.shape()
 		self.total_drop_count = len(self.drops)
 
 	def parse_data(self):
 		with open(self.filename, 'r') as infile:
 			content = infile.read()
 		self.drops = [tuple(map(int, row.split(","))) for row in content.splitlines()]
-
 
 	def print_solution(self):
 		color = "\033[90m" if self.part == 1 else "\033[33m"
@@ -54,20 +79,14 @@ DIRECTIONS = [UP, DOWN, LEFT, RIGHT]
 #####################
 
 def path_finder(input, min_price):
-	def on_board(point):
-		return 0 <= point[0] < input.rows and 0 <= point[1] < input.cols
 
 	def move(point, dir):
 		return (point[0] + dir[0], point[1] + dir[1])
 
-	def is_obstacle(point):
-		return input.grid[point] == "#"
-
-
 	def save_neighbours():
 		for direction in DIRECTIONS:
 			new_point = move(point, direction)
-			if new_point not in visited and on_board(new_point) and not is_obstacle(new_point):
+			if new_point not in visited and input.grid.on_board(new_point) and not input.grid.is_obstacle(new_point):
 				heappush(to_check, (price, new_point))
 		
 	to_check = [(0, input.start)]
@@ -103,8 +122,7 @@ def part2(input):
 	input.part = 2
 
 	def place_new_drop(drop_count):
-		drop_x, drop_y = input.drops[drop_count]
-		input.grid[drop_x][drop_y] = "#"
+		input.grid.add_obstacle(input.drops[drop_count])
 		return drop_count + 1
 
 	drop_count = input.firstx
@@ -127,13 +145,13 @@ INPUT_FILE_NAME = path.join(dirname, "input.txt")
 if __name__ == "__main__":
 
 	try:
-		example = Data(EXAMPLE_FILE_NAME)
+		example = Data(EXAMPLE_FILE_NAME, "Example", (6, 6), 12)
 	except:
 		example = False
 		print("\033[1;91mSave puzzle example to", EXAMPLE_FILE_NAME, "\033[0m", file=sys.stderr)
 
 	try:
-		input = Data(INPUT_FILE_NAME)
+		input = Data(INPUT_FILE_NAME, "Solution", (70, 70), 1024)
 	except:
 		print("\033[1;91mSave puzzle input to",  INPUT_FILE_NAME, "\033[0m", file=sys.stderr)
 		exit(1)
